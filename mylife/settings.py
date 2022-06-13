d64194010c408d4f54050670ee22dab1c10dd4bb
Kazuhiro Ikeda <info@tamias.co.jp>
2022-06-13 22:59:46 +0900

    ページネイション追加

--- mylife/settings.py
+++ mylife/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-^-0o8%7uf-qqw+_91p^uiwqerhbt)+mek-5nb%wh#0@t!ms3w@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,6 +75,13 @@ WSGI_APPLICATION = 'mylife.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#pass : @K`J,_AeqN3uYD()
+"""DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
 
 DATABASES = {
     'default': {
